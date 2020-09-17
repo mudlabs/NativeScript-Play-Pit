5f8b4989e9f135afcc5994fe6432f8c17131afff
Sam Donald <sam@mudlabs.com.au>
2020-09-17 23:33:26 +1000

    update readme project list

--- .github/actions/projects/index.js
+++ .github/actions/projects/index.js
@@ -7,9 +7,11 @@ const utils = require("../utils");
   const projects_dir = await fs.promises.readdir("projects", options);
   const list_regexp = new RegExp(/(- \[(?:\w|\s)+\]\(projects\/(?:\w|\%|\d)+\)\n)+/, "gim");
   
-  console.log(projects_dir);
-  // const list = projects_dir.reduce((prev, curr) => `- [${curr}](${curr})`, "");
-  // const new_readme = current_readme.replace(list_regexp, list);
+  const list = projects_dir.reduce((prev, curr) => {
+    const curr_urlified = curr.replace(/( )/g, "%20");
+    return prev + `- [${curr}](projects/curr_urlified)\n`
+  }, "");
+  const new_readme = current_readme.replace(list_regexp, list);
 
-  // await fs.promises.writeFile("./README.md", new_readme);
+  await fs.promises.writeFile("./README.md", new_readme);
 })()
