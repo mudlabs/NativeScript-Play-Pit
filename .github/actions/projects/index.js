d812d80491ecb63ea5e46db3e977aea1dc79cfc0
Sam Donald <sam@mudlabs.com.au>
2020-09-17 23:26:21 +1000

    add exp

--- .github/actions/projects/index.js
+++ .github/actions/projects/index.js
@@ -5,9 +5,11 @@ const utils = require("../utils");
   const options = { encoding: "utf-8" };
   const current_readme = await fs.promises.readFile("./README.md", options)
   const projects_dir = await fs.promises.readdir("projects", options);
+  const list_regexp = new RegExp(/(- \[(?:\w|\s)+\]\(projects\/(?:\w|\%|\d)+\)\n)+/, "gim");
+  
+  console.log(list);
+  // const list = projects_dir.reduce((prev, curr) => `- [${curr}](${curr})`, "");
+  // const new_readme = current_readme.replace(list_regexp, list);
 
-  const list = projects_dir.reduce((prev, curr) => `- [${curr}](${curr})`, "");
-  const new_readme = current_readme.replace(list_regexp, list);
-
-  await fs.promises.writeFile("./README.md", new_readme);
+  // await fs.promises.writeFile("./README.md", new_readme);
 })()
