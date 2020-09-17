cb8a5b4e269522a6a6c560456058053e37eefbd2
Sam Donald <sam@mudlabs.com.au>
2020-09-18 00:05:55 +1000

    reg

--- .github/actions/projects/index.js
+++ .github/actions/projects/index.js
@@ -5,12 +5,13 @@ const fs = require("fs");
     const options = { encoding: "utf-8", flag: "r" };
     const current_readme = await fs.promises.readFile("README.md", options)
     const projects_dir = await fs.promises.readdir("projects", options);
-    const list_regexp = new RegExp(/(- \[(?:\w|\s)+\]\(projects\/(?:\w|\%|\d)+\)\n)+/, "gim");
+    const list_regexp = new RegExp(/(- \[(?:\w|\s)+\]\((?:\w|\%|\d|\/)+\)\n)+\)\n)+/, "gim");
     
     const list = projects_dir.reduce((prev, curr) => {
       const curr_urlified = curr.replace(/( )/g, "%20");
       return prev + `- [${curr}](projects/${curr_urlified})\n`
     }, "");
+    console.log(list);
     const new_readme = current_readme.replace(list_regexp, list);
     console.log(new_readme);
     await fs.promises.writeFile("README.md", new_readme);
